815884a3247f9c70d0782f3f306b302d8dc193b1
avanish rana <avanish.ranalmp001@gmail.com>
2023-07-13 12:26:14 +0530

    third

--- src/main/java/com/example/Test/TestApplication.java
+++ src/main/java/com/example/Test/TestApplication.java
@@ -3,6 +3,7 @@ package com.example.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -16,6 +17,11 @@ public class TestApplication {
 	@GetMapping("/Home")
 	public String homePage(){
 		return "This is home page and you are welcome";
+	}
+
+	@GetMapping("/Square/{a}")
+	public int square(@PathVariable int a){
+		return a*a;
 	}
 
 }
